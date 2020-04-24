133e81bae37d7b73aa25124dc1e8039ff7da8771
nhattruong1 <nhattruong12c2@gmail.com>
2020-04-24 23:55:31 +0700

    s

--- main.js
+++ main.js
@@ -3,27 +3,26 @@ $.ajax({
     url: "http://demo6370041.mockable.io/getcourses",
     async: true,
     success: function(result) {
-        function buildTable(data){
-            var table = document.getElementById('myTable')
-            for (var i = 0; i <= data.length; i++){
-                var row = `<tr class="header" onclick = "functionX(${data[i].id})">
-                                <td>${data[i].id}</td>
-                                <td>${data[i].name}</td>
-                            </tr>
-                            <tr>
-                                <td colspan = "2" class = "val${data[i].id}"></td>
-                            </tr>
-                            `
-                table.innerHTML += row
-                $('tr.header').click(function(){
-                    $(this).nextUntil('tr.header').css('display', function(i,v){
-                        return this.style.display === 'table-row' ? 'none' : 'table-row';
-                    });
+        var table = document.getElementById('myTable')
+        for (var i = 0; i <= result.data.length; i++){
+            var row = `<tr class="header" onclick = "functionX(${result.data[i].id})">
+                            <td>${result.data[i].id}</td>
+                            <td>${result.data[i].name}</td>
+                        </tr>
+                        <tr>
+                            <td colspan = "2" class = "val${result.data[i].id}"></td>
+                        </tr>`
+            table.innerHTML += row
+            $('tr.header').click(function(){
+                $(this).nextUntil('tr.header').css('display', function(i,v){
+                    return this.style.display === 'table-row' ? 'none' : 'table-row';
                 });
-            }
+            });
         }
-        buildTable(result.data)
-    }              
+    },
+    complete: function(){
+        console.log('done')
+    }
 });
 function functionX(x){
     $.ajax({ 
@@ -36,8 +35,33 @@ function functionX(x){
         success: function(result) {
             var text = $('.val'+x).text("Description: "+result.description +'\n\n'+"Textbook: " +result.textbook)
             text.html(text.html().replace(/\n/g,'<br/>'));
-        }                     
+        },
+        complete: function(){
+        //after completed request then this method will be called.
+        console.log('done')
+    },                  
     });
 }
 
-// Pagination
+// pageSize = 5;
+// var pageCount =  $(".header").length / pageSize;
+//  for(var i = 0 ; i<pageCount;i++){
+    
+//    $("#pagin").append('<li><a href="#">'+(i+1)+'</a></li> ');
+//  }
+//     $("#pagin li").first().find("a").addClass("current")
+// showPage = function(page) {
+//     $(".header").hide();
+//     $(".header").each(function(n) {
+//         if (n >= pageSize * (page - 1) && n < pageSize * page)
+//             $(this).show();
+//     });        
+// }
+
+// showPage(1);
+
+// $("#pagin li a").click(function() {
+//     $("#pagin li a").removeClass("current");
+//     $(this).addClass("current");
+//     showPage(parseInt($(this).text())) 
+// });
